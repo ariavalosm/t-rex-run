c9e94466bbf53f1f35df4c365b382ea5810a68be
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-08-14 20:57:01 -0500

    dino control

---
+++ app.js
@@ -0,0 +1,5 @@
+//this for giving movement thru gravitation
+document.addEventListener("DOMContentLoaded", () =>{
+  const dino = document.querySelector(".dino") //it will jump 150px 
+
+}) //inside this event listener will be a function
