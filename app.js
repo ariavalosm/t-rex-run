1082d20cdd70c0b0caafec014c8be4983cca01cc
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-08-19 12:19:59 -0500

    back to road

--- app.js
+++ app.js
@@ -2,4 +2,22 @@
 document.addEventListener("DOMContentLoaded", () =>{
   const dino = document.querySelector(".dino") //it will jump 150px 
 
+  function control(e) {
+    if (e.keyCode === 32){
+      console.log("pressed")
+    }
+  }
+  document.addEventListener("keyup", control)
 }) //inside this event listener will be a function
+
+if ( position == 150){
+  clearInterval(timerId)
+  console.log("down")
+  let downTimerId = setInterval(function(){
+    if (position == 0) {
+      clearInterval(downTimerId)
+    }
+    position -= 30
+    dino.style.bottom = position + "px"
+  }, 20)
+}
