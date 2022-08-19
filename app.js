b48f891df19cab67776e7cc764f7c6b7c07f1255
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-08-19 13:42:24 -0500

    up and down done

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 //this for giving movement thru gravitation
 document.addEventListener("DOMContentLoaded", () =>{
   const dino = document.querySelector(".dino") //it will jump 150px 
+  let isJumping = false
 
   function control(e) {
     if (e.keyCode === 32){
-      console.log("pressed")
+      if (!isJumping){
+        isJumping = true
+        jump()
+      }
     }
   }
   document.addEventListener("keyup", control)
-}) //inside this event listener will be a function
+//inside this event listener will be a function
 
-if ( position == 150){
-  clearInterval(timerId)
-  console.log("down")
-  let downTimerId = setInterval(function(){
-    if (position == 0) {
-      clearInterval(downTimerId)
+function jump(){
+  let position = 0
+  let timerId = setInterval(function() {
+    
+    //Dino jumps yay
+    if ( position == 150){
+      clearInterval(timerId)
+      console.log("down")
+      let downTimerId = setInterval(function(){
+        if (position == 0) {
+          clearInterval(downTimerId)
+          isJumping = false
+        }
+        position -= 5
+        count--
+        position = position * gravity
+        dino.style.bottom = position + "px"
+      }, 20)
     }
-    position -= 30
-    dino.style.bottom = position + "px"
-  }, 20)
-}
+
+    //move up
+    position +=30
+    count++
+    position = position * gravity
+    dino.style.bottom = position + 'px'
+  },20)
+
+  }
+  
+})
+
+
